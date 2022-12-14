de7908f8f520820f48662016f9fefc2867dca683
Avishkar Jadhav <avishkar9561@gmail.com>
2022-12-14 16:58:25 +0530

    Added 5 line of spaces in Method ClickOnSubmitMoNo()

--- src/main/java/com/neoStox/pageObjects/MobileNoPage.java
+++ src/main/java/com/neoStox/pageObjects/MobileNoPage.java
@@ -28,6 +28,11 @@ public class MobileNoPage {
 	
 	public void ClickOnSubmitMObno() throws InterruptedException
 	{
+		
+		
+		
+		
+		
 		Thread.sleep(1000);
 		SubmitMOBNO.click();
 		Thread.sleep(1000);
